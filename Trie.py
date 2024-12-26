19d273fc802c8d37a0c58a881576c7d5530cc103
Igor <igorspinoladev@gmail.com>
2024-12-26 11:27:59 -0300

    consulta, rankear methods added

--- Trie.py
+++ Trie.py
@@ -1,9 +1,11 @@
+from typing import Dict, List
 from No import No
 
 class Trie:
     def __init__(self) -> None:
         self.raiz: No = No()
         i: int = 97
+        self.contador: Dict[str, int] = {}
 
         while i < 123:
            self.raiz.ref.append(No(chr(i)))
@@ -21,6 +23,50 @@ class Trie:
             if atual.ref[indice].chave == "":
                 atual.ref[indice] = No(le)
                 output = "palavra inserida: " + palavra
+                if not self.contador.get(palavra):
+                    self.contador[palavra] = 0
             atual = atual.ref[indice]
-
         return output
+
+    def consulta(self, palavra: str) -> str:
+
+        output: str = ""
+        atual: No = self.raiz
+        j: int = 0
+
+        for le in palavra:
+            indice: int = ord(le) - ord('a')
+            if len(atual.ref) == 0 and j == len(palavra) - 1:
+                if not self.contador.get(palavra):
+                    self.contador[palavra] = 0
+                self.contador[palavra] += 1
+                output = "palavra existente: " + palavra + " " + str(self.contador[palavra])
+                return output
+            if len(atual.ref) == 0 and j != len(palavra)- 1:
+                output = "palavra inexistente: " + palavra
+                return output
+            if atual.ref[indice].chave == "":
+                output = "palavra inexistente: " + palavra
+                return output
+
+            atual = atual.ref[indice]
+            j += 1
+
+        if not self.contador.get(palavra):
+            self.contador[palavra] = 0
+        self.contador[palavra] += 1
+        output = "palavra existente: " + palavra + " " + str(self.contador[palavra])
+        return output
+
+    def rankear(self) -> List:
+        max: int = 0
+        chaves: List[str] = []
+        for e in self.contador:
+            valor = self.contador[e]
+            if max < valor:
+                max = valor
+        for e in self.contador:
+            if self.contador[e] == max:
+                chaves.append(e)
+        chaves.sort()
+        return [chaves, max]
--- trab01.py
+++ trab01.py
@@ -1,24 +1,38 @@
 # Versao do Python: 3.10.12
 
+from typing import List
 from Trie import Trie
 
 tr: Trie = Trie()
+
 #print(*tr.raiz.ref)
 #for n in tr.raiz.ref:
 #    print("chave:" + n.chave)
 #print("raiz:" + tr.raiz.chave)
+
+# guardar resultados pra printar tudo junto no final
 
 entrada: str = input()
 
 while entrada != 'e':
 
     if entrada == 'i':
-        arg = input()
-        print(tr.insere(arg))
+        arg: str = input()
+        result_i: str = tr.insere(arg)
+        print(result_i)
     elif entrada == 'c':
-        pass
+        arg: str = input()
+        result_c: str = tr.consulta(arg)
+        print(result_c)
     elif entrada == 'f':
-        pass
+        result_f: List = tr.rankear()
+        if len(result_f[0]) == 0:
+            print("trie vazia")
+        else:
+            print("palavras mais consultadas:")
+            for e in result_f[0]:
+                print(e)
+            print("numero de acessos: " + str(result_f[1]))
     elif entrada == 'p':
         pass
     else:
